606d530867a73259f76765162db5d0dfed25c017
Gabril61 <ricciobravo61@gmail.com>
2026-06-01 11:36:31 -0400

    Respaldo Automatico: lun. 01/06/2026 11:36:31,44

--- Respaldar_Nube.bat
+++ Respaldar_Nube.bat
@@ -12,7 +12,15 @@ git commit -m "Respaldo Automatico: %date% %time%"
 git push origin main
 
 echo.
+echo Sincronizando patrones con el Bot Generador local...
+xcopy /Y /D "*.val" "..\App_Formulario\TextilFit_Bot\Maestros\"
+xcopy /Y /D "*.smis" "..\App_Formulario\TextilFit_Bot\Maestros\"
+echo.
 echo ========================================================
 echo       RESPALDO COMPLETADO EXITOSAMENTE
+echo Sincronizando patrones con el Bot Generador local...
+xcopy /Y /D "*.val" "..\App_Formulario\TextilFit_Bot\Maestros\"
+xcopy /Y /D "*.smis" "..\App_Formulario\TextilFit_Bot\Maestros\"
+echo.
 echo ========================================================
 timeout /t 5
